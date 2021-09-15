f5084184473af5561ce11591530fd3c0927cd245
amoresjan <amoresjan14@gmail.com>
2021-09-15 15:57:50 +0800

    add admin.py

--- backend/itrabaho/admin.py
+++ backend/itrabaho/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from backend.itrabaho import models
 
 # Register your models here.
 
-admin.site.register(User)
+admin.site.register(models.UserModel)
+admin.site.register(models.LGURepresentativeModel)
+admin.site.register(models.RecruiterModel)
+admin.site.register(models.ProfileModel)
+admin.site.register(models.ExperienceModel)
+admin.site.register(models.ExperienceDetailModel)
+admin.site.register(models.ApplicantModel)
