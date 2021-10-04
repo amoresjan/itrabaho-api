5d61769b77538784b3c2ee5c61bcc8780c37229b
amoresjan <amoresjan14@gmail.com>
2021-10-05 01:42:16 +0800

    added usertype

--- backend/itrabaho/serializers/base.py
+++ backend/itrabaho/serializers/base.py
@@ -6,28 +6,63 @@ from backend.itrabaho import models
 class UserModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.UserModel
-        fields = "__all__"
+        fields = [
+            "id",
+            "last_login",
+            "firstName",
+            "lastName",
+            "phoneNumber",
+            "userType",
+        ]
 
 
 class ApplicantsModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.ApplicantModel
-        fields = "__all__"
+        fields = [
+            "id",
+            "last_login",
+            "firstName",
+            "lastName",
+            "phoneNumber",
+            "userType",
+        ]
 
 
 class JobPostModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.JobPostModel
-        fields = "__all__"
+        fields = [
+            "id",
+            "last_login",
+            "firstName",
+            "lastName",
+            "phoneNumber",
+            "userType",
+        ]
 
 
 class RecruiterModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.RecruiterModel
-        fields = "__all__"
+        fields = [
+            "id",
+            "last_login",
+            "firstName",
+            "lastName",
+            "phoneNumber",
+            "userType",
+        ]
 
 
 class ReviewModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.ReviewModel
-        fields = "__all__"
+        fields = [
+            "id",
+            "last_login",
+            "firstName",
+            "lastName",
+            "phoneNumber",
+            "userType",
+        ]
