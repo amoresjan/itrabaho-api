462fbd08cee70ba33df8ffc77f77fe3563ed5c5f
amoresjan <amoresjan14@gmail.com>
2021-10-05 00:58:52 +0800

    changed databases

--- backend/settings.py
+++ backend/settings.py
@@ -98,9 +98,14 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default="postgres://postgres@localhost/itrabaho_db"
-    )
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "itrabaho_db",
+        "USER": "itrabaho",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": "",
+    }
 }
 
 
